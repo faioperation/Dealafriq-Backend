9856ee5b2dcd92982f03abfa62386529411427a9
ahanaf607307 <ahanaf607307@gmail.com>
2026-02-20 10:54:42 +0600

    fix  database url and redis url issue

--- src/app/config/dbConnect.js
+++ src/app/config/dbConnect.js
@@ -7,17 +7,12 @@ dotenv.config();
 const connectDB = async () => {
   try {
     const pool = new Pool({
-      host: envVars.DB_HOST,
-      user: envVars.DB_USER,
-      password: envVars.DB_PASSWORD,
-      database: envVars.DB_NAME,
-      port: envVars.DB_PORT || 5432,
+     url: envVars.DATABASE_URL,
     });
 
     const client = await pool.connect();
     console.log("✅ PostgreSQL Connected Successfully!");
     client.release();
-
     return pool;
   } catch (error) {
     console.error("❌ Database connection failed:", error.message);
