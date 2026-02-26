314b1d8c82915e1a8ed9f11bd986eaed18fa6254
ahanaf607307 <ahanaf607307@gmail.com>
2026-02-26 12:56:58 +0600

    fix speaker and message issue

--- src/app/config/uploadTranscript.js
+++ src/app/config/uploadTranscript.js
@@ -5,7 +5,7 @@ import fs from "fs";
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const projectId = req.body.projectId;
-    let uploadPath = "/uploads/transcripts";
+    let uploadPath = path.join(process.cwd(), "uploads", "transcripts");
 
     if (projectId) {
       uploadPath = path.join(uploadPath, projectId);
